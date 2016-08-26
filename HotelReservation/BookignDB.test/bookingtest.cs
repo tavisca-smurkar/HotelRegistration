e1019dd93b2213db970524c1c125132ca4e4ca5e
tavisca-smurkar <smurkar@tavisca.com>
2016-08-26 10:01:55 +0530

    HotelRooms and BookingDetails  tables added with insert ans select functionality

---
+++ HotelReservation/BookignDB.test/bookingtest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BookingDetailsOperation.Data;
+
+namespace BookignDB.test
+{
+    [TestClass]
+    public class bookingtest
+    {
+        [TestMethod]
+        public void TestBookingDetailsInsert()
+        {
+            BookingDetailsDBImpl test = new BookingDetailsDBImpl();
+            Int64 result = test.InsertBookingDetails(1, 2);
+            Assert.AreNotEqual(result, 0);
+        }
+    }
+}
---
+++ HotelReservation/HotelReservation.Entities/HotelRooms.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservation.Entities
+{
+    public class HotelRooms
+    {
+        public Int64 Room_Id { get; set; }
+        public Int64 Hotel_Id { get; set; }
+        public string RoomType { get; set; }
+        public Int64 AvailableRooms { get; set; }
+        public Int64 TotalRooms { get; set; }
+
+    }
+}
